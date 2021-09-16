499b0b8ac01770ce954993dfbbc00dba1d8e1c3c
Матвей <whitewolf.mot185@gmail.com>
2021-09-17 02:13:54 +0300

    Упростил: теперь функция вызывается без параметров и нет сторонних библиотек

--- lesson1/main.go
+++ lesson1/main.go
@@ -2,82 +2,50 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 const (
 	PARAMETERS = 3
 )
 
-func createPic(values []string) {
+var values map[string]int32
+
+func createPic() {
 	char := "X"
 	times := 1
 	size := 15
-
 	if len(values) > 0 {
-		for _, value := range values {
-			var (
-				param string
-				i     int
-			)
+		value, ok := values["char"]
+		if ok {
+			char = string(value)
+		}
 
-			for i = 0; value[i] != '='; i++ {
-				param += string(value[i])
-			}
-			i++
+		value, ok = values["size"]
+		if ok {
+			size = int(value)
+		}
 
-			switch param {
-			case "size":
-				var res string
-				for ; i < len(value); i++ {
-					res += string(value[i])
-				}
-				val, err := strconv.Atoi(res)
-				if err == nil {
-					size = val
-				} else {
-					fmt.Println("Что-то не так с цифрами")
-				}
+		value, ok = values["times"]
+		if ok {
+			times = int(value)
+		}
 
-				if size > 0 && size%2 == 1 {
-					fmt.Println("size = ", size)
-				} else {
-					size = 15
-					fmt.Println("Значение сброшено до начального. Size = 15")
-				}
+		if size > 0 && size%2 == 1 {
+			fmt.Println("size = ", size)
+		} else {
+			size = 15
+			fmt.Println("Значение сброшено до начального. Size = 15")
+		}
 
-			case "char":
-				char = string(value[i])
-				fmt.Println("Поле char введено. Сhar = ", char)
+		fmt.Println("Сhar = ", char)
 
-			case "times":
-				var res string
-				for ; i < len(value); i++ {
-					res += string(value[i])
-				}
-				val, err := strconv.Atoi(res)
-				if err == nil {
-					times = val
-				} else {
-					fmt.Println("Что-то не так с цифрами")
-				}
-
-				if times > 0 {
-					fmt.Println("Поле times введено. Times = ", times)
-				} else {
-					times = 1
-					fmt.Println("Значение сброшено до начального. Times = 1")
-				}
-
-			default:
-				fmt.Println("Значения сброшены до начальных")
-				times = 1
-				size = 15
-				char = "X"
-			}
+		if times > 0 {
+			fmt.Println("Times = ", times)
+		} else {
+			times = 1
+			fmt.Println("Значение сброшено до начального. Times = 1")
 		}
 	}
-
 	var result string
 	for k := 0; k < times; k++ {
 		for i := 0; i < size; i++ {
@@ -100,16 +68,15 @@ func createPic(values []string) {
 }
 
 func main() {
+	values = make(map[string]int32)
 	fmt.Printf("Здравствтуй пользователь!\nДля работы данной программы можно ввести %d параметра:\n1.size (вводите, пожалуйста, только нечетные числа больше 0)\n2.char\n3.times\n"+
-		"Чтобы инициализировать параметры, вводите их название и число в формате <name>=<number || char>. Верно будет последнее введенное значение.\nЕсли вы хотите закончить ввод, напишите end\n", PARAMETERS)
+		"Чтобы инициализировать параметры, вводите их название и число в формате <name> <number || char>. Верно будет последнее введенное значение.\nЕсли вы хотите закончить ввод, напишите end\n", PARAMETERS)
 	// command
 	var (
 		param string
+		char  rune
+		num   int32
 	)
-
-	// options
-
-	var args []string
 
 	for {
 		_, err := fmt.Scanf("%s", &param)
@@ -121,8 +88,23 @@ func main() {
 			break
 		}
 
-		args = append(args, param)
+		switch param {
+		case "char":
+			_, err := fmt.Scanf("%c\n", &char)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			values["char"] = char
+
+		default:
+			_, err := fmt.Scanf("%d", &num)
+			if err != nil {
+				fmt.Println(err)
+			}
+			values[param] = num
+		}
 	}
 
-	createPic(args)
+	createPic()
 }
